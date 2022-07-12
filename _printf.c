804033b88e0239493b23fcc5f617faba7208a3b4
yberhanu <yohannes.birh@gmail.com>
2022-07-12 14:24:21 -0700

    error solved

--- _printf.c
+++ _printf.c
@@ -1,40 +1,49 @@
-#include <unistd.h>
 #include "main.h"
 /**
-*_printf - takes in a string and prints different types of arguments for
-* an unspecified amount of arguments
-* @format: the initial string that tell us what is going to be printed
-* Return: the amount of times we write to stdout
+* _printf - formatted output conversion and print data.
+* @format: input string.
+*
+* Return: number of chars printed.
 */
 int _printf(const char *format, ...)
 {
-int i, count;
-int (*f)(va_list);
-va_list list;
-if (format == NULL)
+unsigned int i = 0, len = 0, ibuf = 0;
+va_list arguments;
+int (*function)(va_list, char *, unsigned int);
+char *buffer;
+va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
+if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
 return (-1);
-va_start(list, format);
-i = count = 0;
-while (format[i] != '\0')
+if (!format[i])
+return (0);
+for (i = 0; format && format[i]; i++)
 {
 if (format[i] == '%')
 {
 if (format[i + 1] == '\0')
+{print_buf(buffer, ibuf), free(buffer), va_end(arguments);
 return (-1);
-f = get_func(format[i + 1]);
-if (f == NULL)
-count += print_nan(format[i], format[i + 1]);
+}
 else
-count += f(list);
-i++;
+{function = get_print_func(format, i + 1);
+if (function == NULL)
+{
+if (format[i + 1] == ' ' && !format[i + 2])
+return (-1);
+handl_buf(buffer, format[i], ibuf), len++, i--;
 }
 else
 {
-_putchar(format[i]);
-count++;
+len += function(arguments, buffer, ibuf);
+i += ev_print_func(format, i + 1);
 }
-i++;
+} i++;
 }
-va_end(list);
-return (count);
+else
+handl_buf(buffer, format[i], ibuf), len++;
+for (ibuf = len; ibuf > 1024; ibuf -= 1024)
+;
+}
+print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+return (len);
 }
