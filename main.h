a7c67ddf23c0d15afdec6f7d5e4157508030ec8e
yberhanu <yohannes.birh@gmail.com>
2022-07-12 13:05:47 -0700

    edit header

--- main.h
+++ main.h
@@ -3,19 +3,17 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
-
 /**
- * struct buffer - buffer structure for our implimentation of printf
- * @buf: buffer to write characters
- * @tmpbuf: tmp buffer to write to before putting in buffer
- * @format: the string passed to our printf
- * @ap: the variadic address point
- * @bp: the current point in the buffer
- * @tp: the current point in the tmp buffer
- * @fp: the current point in the format
- * @printed: the number of chars printed from _write
- */
-
+* struct buffer - buffer structure for our implimentation of printf
+* @buf: buffer to write characters
+* @tmpbuf: tmp buffer to write to before putting in buffer
+* @format: the string passed to our printf
+* @ap: the variadic address point
+* @bp: the current point in the buffer
+* @tp: the current point in the tmp buffer
+* @fp: the current point in the format
+* @printed: the number of chars printed from _write
+*/
 typedef struct buffer
 {
 char *buf;
@@ -27,20 +25,18 @@ int tp;
 int fp;
 unsigned int printed;
 } buffer;
-
 /**
- *struct handlers - handlers struct
- *@plus:'+' flag
- *@space:' ' flag
- *@hash:'#' flag
- *@zero:'0' flag
- *@minus:'-' handler
- *@width:width handler
- *@precision:precision handler
- *@h_mod:h modifier handler
- *@l_mod:l modifier handler
- */
-
+*struct handlers - handlers struct
+*@plus:'+' flag
+*@space:' ' flag
+*@hash:'#' flag
+*@zero:'0' flag
+*@minus:'-' handler
+*@width:width handler
+*@precision:precision handler
+*@h_mod:h modifier handler
+*@l_mod:l modifier handler
+*/
 typedef struct handlers
 {
 unsigned int plus;
@@ -53,19 +49,17 @@ unsigned int precision;
 unsigned int h_mod;
 unsigned int l_mod;
 } han_s;
-
 /**
- * struct ops - a structure containing a char to compare with formatb modifiers
- * and then choose the right function when it matches
- * @operation: the char we want to compare to
- * @func: the address of the function we want to return if a char matches
- */
-
+* struct ops - a structure containing a char to compare with formatb modifiers
+* and then choose the right function when it matches
+* @operation: the char we want to compare to
+* @func: the address of the function we want to return if a char matches
+*/
 typedef struct ops
 {
 char operation;
 int (*func)(va_list);
-}ops_f;
+} ops_f;
 int _printf(const char *format, ...);
 int _putchar(char c);
 int (*get_func(char ch))(va_list);
