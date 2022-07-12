d67ae2b795a1f2b40ccfad39aefaa7a60ff66c46
kirubelAtnafu <kiraatnafu@gmail.com>
2022-07-12 14:43:53 -0700

    putchar

--- _printf.c
+++ _printf.c
@@ -1,79 +1,40 @@
+#include <unistd.h>
 #include "main.h"
-void cleanup(va_list args, buffer_t *output);
-int run_printf(const char *format, va_list args, buffer_t *output);
-int _printf(const char *format, ...);
 /**
-* cleanup - Peforms cleanup operations for _printf.
-* @args: A va_list of arguments provided to _printf.
-* @output: A buffer_t struct.
-*/
-void cleanup(va_list args, buffer_t *output)
-{
-va_end(args);
-write(1, output->start, output->len);
-free_buffer(output);
-}
-/**
-* run_printf - Reads through the format string for _printf.
-* @format: Character string to print - may contain directives.
-* @output: A buffer_t struct containing a buffer.
-* @args: A va_list of arguments.
-*
-* Return: The number of characters stored to output.
-*/
-int run_printf(const char *format, va_list args, buffer_t *output)
-{
-int i, wid, prec, ret = 0;
-char tmp;
-unsigned char flags, len;
-unsigned int (*f)(va_list, buffer_t *,
-unsigned char, int, int, unsigned char);
-for (i = 0; *(format + i); i++)
-{
-len = 0;
-if (*(format + i) == '%')
-{
-tmp = 0;
-flags = handle_flags(format + i + 1, &tmp);
-wid = handle_width(args, format + i + tmp + 1, &tmp);
-prec = handle_precision(args, format + i + tmp + 1,&tmp);
-len = handle_length(format + i + tmp + 1, &tmp);
-f = handle_specifiers(format + i + tmp + 1);
-if (f != NULL)
-{
-i += tmp + 1;
-ret += f(args, output, flags, wid, prec, len);
-continue;
-}
-else if (*(format + i + tmp + 1) == '\0')
-{
-ret = -1;
-break;
-}
-}
-ret += _memcpy(output, (format + i), 1);
-i += (len != 0) ? 1 : 0;
-}
-cleanup(args, output);
-return (ret);
-}
-/**
-* _printf - Outputs a formatted string.
-* @format: Character string to print - may contain directives.
-*
-* Return: The number of characters printed.
-*/
+ *_printf - takes in a string and prints different types of arguments for
+ * an unspecified amount of arguments
+ * @format: the initial string that tell us what is going to be printed
+ * Return: the amount of times we write to stdout
+ */
 int _printf(const char *format, ...)
 {
-buffer_t *output;
-va_list args;
-int ret;
+int i, count;
+int (*f)(va_list);
+va_list list;
 if (format == NULL)
 return (-1);
-output = init_buffer();
-if (output == NULL)
+va_start(list, format);
+i = count = 0;
+while (format[i] != '\0')
+{
+if (format[i] == '%')
+{
+if (format[i + 1] == '\0')
 return (-1);
-va_start(args, format);
-ret = run_printf(format, args, output);
-return (ret);
+f = get_func(format[i + 1]);
+if (f == NULL)
+count += print_nan(format[i], format[i + 1]);
+else
+count += f(list);
+i++;
+}
+else
+{
+_putchar(format[i]);
+count++;
+}
+i++;
+}
+va_end(list);
+return (count);
 }
