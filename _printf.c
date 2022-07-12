7c4a26236aae3a5f429c2bbaa408a435efb1f3f2
yberhanu <yohannes.birh@gmail.com>
2022-07-12 14:34:52 -0700

    error solved

--- _printf.c
+++ _printf.c
@@ -1,49 +1,79 @@
 #include "main.h"
+void cleanup(va_list args, buffer_t *output);
+int run_printf(const char *format, va_list args, buffer_t *output);
+int _printf(const char *format, ...);
 /**
-* _printf - formatted output conversion and print data.
-* @format: input string.
+* cleanup - Peforms cleanup operations for _printf.
+* @args: A va_list of arguments provided to _printf.
+* @output: A buffer_t struct.
+*/
+void cleanup(va_list args, buffer_t *output)
+{
+va_end(args);
+write(1, output->start, output->len);
+free_buffer(output);
+}
+/**
+* run_printf - Reads through the format string for _printf.
+* @format: Character string to print - may contain directives.
+* @output: A buffer_t struct containing a buffer.
+* @args: A va_list of arguments.
 *
-* Return: number of chars printed.
+* Return: The number of characters stored to output.
+*/
+int run_printf(const char *format, va_list args, buffer_t *output)
+{
+int i, wid, prec, ret = 0;
+char tmp;
+unsigned char flags, len;
+unsigned int (*f)(va_list, buffer_t *,
+unsigned char, int, int, unsigned char);
+for (i = 0; *(format + i); i++)
+{
+len = 0;
+if (*(format + i) == '%')
+{
+tmp = 0;
+flags = handle_flags(format + i + 1, &tmp);
+wid = handle_width(args, format + i + tmp + 1, &tmp);
+prec = handle_precision(args, format + i + tmp + 1,&tmp);
+len = handle_length(format + i + tmp + 1, &tmp);
+f = handle_specifiers(format + i + tmp + 1);
+if (f != NULL)
+{
+i += tmp + 1;
+ret += f(args, output, flags, wid, prec, len);
+continue;
+}
+else if (*(format + i + tmp + 1) == '\0')
+{
+ret = -1;
+break;
+}
+}
+ret += _memcpy(output, (format + i), 1);
+i += (len != 0) ? 1 : 0;
+}
+cleanup(args, output);
+return (ret);
+}
+/**
+* _printf - Outputs a formatted string.
+* @format: Character string to print - may contain directives.
+*
+* Return: The number of characters printed.
 */
 int _printf(const char *format, ...)
 {
-unsigned int i = 0, len = 0, ibuf = 0;
-va_list arguments;
-int (*function)(va_list, char *, unsigned int);
-char *buffer;
-va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
-if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
+buffer_t *output;
+va_list args;
+int ret;
+if (format == NULL)
 return (-1);
-if (!format[i])
-return (0);
-for (i = 0; format && format[i]; i++)
-{
-if (format[i] == '%')
-{
-if (format[i + 1] == '\0')
-{print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+output = init_buffer();
+if (output == NULL)
 return (-1);
-}
-else
-{function = get_print_func(format, i + 1);
-if (function == NULL)
-{
-if (format[i + 1] == ' ' && !format[i + 2])
-return (-1);
-handl_buf(buffer, format[i], ibuf), len++, i--;
-}
-else
-{
-len += function(arguments, buffer, ibuf);
-i += ev_print_func(format, i + 1);
-}
-} i++;
-}
-else
-handl_buf(buffer, format[i], ibuf), len++;
-for (ibuf = len; ibuf > 1024; ibuf -= 1024)
-;
-}
-print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-return (len);
+va_start(args, format);
+ret = run_printf(format, args, output);
+return (ret);
 }
